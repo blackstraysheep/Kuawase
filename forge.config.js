fc68613c4d5993ba2a95bad43f4f92ea506b82ad
Kengo T <kengo.nd77@gmail.com>
2025-04-28 01:53:00 +0900

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: favicon.ico // アイコンのパスを指定
+    icon: path.join(__dirname, 'favicon.ico')
   },
   rebuildConfig: {},
   makers: [
