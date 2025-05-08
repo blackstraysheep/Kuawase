a82817ea4f3ee38b40066f1aaaceeb43facb3143
Kengo T <kengo.nd77@gmail.com>
2025-05-08 22:52:55 +0900

    css色指定の調整

---
+++ js/color.js
@@ -0,0 +1,47 @@
+const THEMES = {
+    yellow: {
+      primary:      '#fffc2e',  // メイン（淡い黄色）
+      'primary-dark':  '#ffec00',  // 濃い目
+      'primary-light':'#fff78e'   // 薄い目
+    },
+    blue: {
+      primary:      '#a3d5ff',  // メイン（読みやすい水色）
+      'primary-dark':  '#61b2ff',  // 濃い目
+      'primary-light':'#d8eeff'   // 薄い目
+    },
+    green: {
+      primary:      '#a8e6cf',  // メイン（柔らかい緑）
+      'primary-dark':  '#81c784',
+      'primary-light':'#cfffd3'
+    },
+    red: {
+      primary:      '#ff9e9e',  // メイン（淡い赤）
+      'primary-dark':  '#ff6e6e',
+      'primary-light':'#ffd4d4'
+    },
+    purple: {
+      primary:      '#d1c4e9',  // メイン（淡い紫）
+      'primary-dark':  '#b39ddb',
+      'primary-light':'#e8dffb'
+    },
+    orange: {
+      primary:      '#ffd9a3',  // メイン（パステルオレンジ）
+      'primary-dark':  '#ffb84d',
+      'primary-light':'#ffeecc'
+    },
+    teal: {
+      primary:      '#a2d5c6',  // メイン（淡い青緑）
+      'primary-dark':  '#67bdb3',
+      'primary-light':'#d6f1ef'
+    },
+    pink: {
+      primary:      '#ffb3c1',  // メイン（柔らかいピンク）
+      'primary-dark':  '#ff7b91',
+      'primary-light':'#ffdbea'
+    },
+    mint: {
+      primary:      '#b2f2bb',  // メイン（ミントグリーン）
+      'primary-dark':  '#8ce99a',
+      'primary-light':'#e2fcec'
+    }
+};
